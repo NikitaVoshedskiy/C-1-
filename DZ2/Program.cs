aa3ff84a71a0c8cd44f65cafb710dabba5d08ac1
Nikita <voshedskiy99@mail.ru>
2023-09-27 00:33:57 +0300

    Сделал дз после первого семинара

---
+++ DZ2/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите число №1:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число №2:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число №3:");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+Вариант №1
+int max = num1;
+
+if (num1>max) max = num1;
+if (num2>max) max = num2;
+if (num3>max) max = num3;
+Console.WriteLine($"{max} самое большое число!");
+
+//Вариант №2
+
+/*if (num1 < num2 && num3 > num2)
+{
+    Console.WriteLine("num3 самое большое число");
+}
+if (num1 < num2 && num2 > num3)
+{
+    Console.WriteLine("num2 самое большое число");
+}
+if (num2 < num1 && num2 > num3)
+{
+    Console.WriteLine("num1 самое большое число");
+}
+*/
---
+++ DZ4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = 1;
+
+if (num1 > num2)
+{
+while (num2<=num1)
+{
+    if (num2 % 2 == 0)
+    {
+      Console.Write($"{num2}, ");
+    }
+    num2++;
+}
+}
+else
+{
+    Console.WriteLine("Введите число болше одного");
+}

07e3793c8dbc783d99e1214ed748e4fe952a852f
Nikita <voshedskiy99@mail.ru>
2023-09-26 23:11:54 +0300

    Семинар1

---
+++ task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа принимает два числа и проверяет, является ли первое число квадрату второго
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Квадрат вашего числа: ");
+int square = Convert.ToInt32(Console.ReadLine());
+if (number * number == square)
+{
+    Console.WriteLine("True");
+    Console.Read();
+}
+else
+{
+    Console.WriteLine("false");
+}
---
+++ task3/Program.cs
@@ -0,0 +1,87 @@
+﻿// Написали программу, оторая при вводе числа выдает день недели
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <1 || number > 7) // || - или , && - и
+{
+    Console.WriteLine("Такого дня недели нет");
+}
+else
+{
+if (number == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (number == 2)
+{
+    Console.WriteLine("Понедельник");
+}
+if (number == 3)
+{
+    Console.WriteLine("Понедельник");
+}
+if (number == 4)
+{
+    Console.WriteLine("Понеельник");
+}
+if (number == 5)
+{
+    Console.WriteLine("Понедельник");
+}
+if (number == 6)
+{
+    Console.WriteLine("Понедльник");
+}
+if (number == 7)
+{
+    Console.WriteLine("Понедельк");
+}
+}
+
+// Вариант 2
+/*
+switch (nomer)
+{
+case 1:
+{
+Console.WriteLine("понедельник");
+break;
+}
+case 2:
+{
+Console.WriteLine("вторник");
+break;
+}
+case 3:
+{
+Console.WriteLine("среда");
+break;
+}
+case 4:
+{
+Console.WriteLine("четверг");
+break;
+}
+case 5:
+{
+Console.WriteLine("пятница");
+break;
+}
+case 6:
+{
+Console.WriteLine("суббота");
+break;
+}
+case 7:
+{
+Console.WriteLine("воскресенье");
+break;
+}
+default:
+{
+Console.WriteLine("нет такого дня недели");
+break;
+}
+}
+*/
